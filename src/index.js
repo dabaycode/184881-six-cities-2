d443cbb9cd8c460ae6250b051f42a57b7e4490ce
Dmitry Bayko <dabayko@gmail.com>
2019-10-21 20:48:05 +1000

    Created main component, render 4 rents

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import ReactDOM from 'react-dom';
 import App from 'Components/app/app';
 
+const rentListTitles = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`, `Wood and stone place`];
+
+const shuffleRentArray = (array) => {
+
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array.slice(0, 4);
+};
+
 const init = () => {
-  ReactDOM.render(<App/>, document.getElementById(`root`));
+  ReactDOM.render(<App rentList={shuffleRentArray(rentListTitles)}/>, document.getElementById(`root`));
 };
 
 init();
